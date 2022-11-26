4fb0ab67439039288bf35bdbcfd5503715ce0bf7
luisfilipeap <luisfilipeap@gmail.com>
2022-11-26 12:58:46 -0300

    Arquivos iniciais do hello_world em C++

---
+++ Hello_World/hello_world.cpp
@@ -0,0 +1,15 @@
+#include "hello_world.h"
+
+using namespace std;
+
+
+
+string hello_world::hello()
+{
+    return "Hello World";
+}
+
+
+
+
+
---
+++ Hello_World/main.cpp
@@ -0,0 +1,9 @@
+#include "hello_world.h"
+#include <iostream>
+
+
+int main()
+{
+    std::cout << hello_world::hello() << std::endl;
+    return 0;
+}
